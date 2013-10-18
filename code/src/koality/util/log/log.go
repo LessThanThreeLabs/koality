787874a5169ff7a6c9a4f7af7f307a04a338a576
Andrey Kostov <kostov.andrey@gmail.com>
2013-10-18 11:00:18 -0700

    Log modifications

--- code/src/koality/util/log/log.go
+++ code/src/koality/util/log/log.go
@@ -1,18 +1,28 @@
 package log
 
 import (
+	"fmt"
 	"github.com/scale-it/go-log"
 	"log/syslog"
 )
 
-var fileName = "log.txt"
-var formatter = log.StdFormatter{"[root]", log.Lmicroseconds | log.Lshortfile, false}
-var maxFileSize = 1 << 20
+var (
+	// TODO(andrey) change to where we want the log files to go
+	fileName    = "log.txt"
+	formatter   = log.StdFormatter{"[root]", log.Lmicroseconds | log.Lshortfile, false}
+	maxFileSize = 1 << 20
+
+	counter = 0
+)
 
 var Logger *log.Logger
 
 func init() {
 	Logger = log.New()
+
+	counter++
+
+	fmt.Printf("%s\n", counter)
 
 	// TODO(andrey) change myprog to deployment address or license key
 	logwriter, _ := syslog.New(syslog.LOG_NOTICE, "my_program")
@@ -22,4 +32,24 @@ func init() {
 	logFile, _ := log.NewRotFile(fileName, false, maxFileSize, 0)
 
 	Logger.AddHandler(&logFile, log.Levels.Trace, formatter)
+}
+
+func Debug(v ...interface{}) {
+	Logger.Log(log.Levels.Debug, fmt.Sprintln(v...))
+}
+
+func Info(v ...interface{}) {
+	Logger.Log(log.Levels.Info, fmt.Sprintln(v...))
+}
+
+func Warning(v ...interface{}) {
+	Logger.Log(log.Levels.Warning, fmt.Sprintln(v...))
+}
+
+func Error(v ...interface{}) {
+	Logger.Log(log.Levels.Error, fmt.Sprintln(v...))
+}
+
+func Critical(v ...interface{}) {
+	Logger.Log(log.Levels.Critical, fmt.Sprintln(v...))
 }
