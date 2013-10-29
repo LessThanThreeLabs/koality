bd25759b6b49000086be58d809e555f6f3df2fdd
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-10-29 16:04:34 -0700

    Make combineReaders propagate IO errors and properly handle combined EOF

--- code/back/src/koality/util/ioutil/ioutil.go
+++ code/back/src/koality/util/ioutil/ioutil.go
@@ -6,35 +6,77 @@ import (
 	"sync"
 )
 
-func CombineReaders(readers ...io.Reader) io.Reader {
-	combinedOut := new(bytes.Buffer)
+type combinedReader struct {
+	buffer    *bytes.Buffer
+	readers   []io.Reader
+	locker    sync.Mutex
+	exitError error
+}
 
-	mutex := new(sync.Mutex)
-	for _, reader := range readers {
-		go syncCopy(reader, combinedOut, mutex)
+func CombineReaders(readers ...io.Reader) io.Reader {
+	combinedOut := &combinedReader{
+		buffer:  new(bytes.Buffer),
+		locker:  new(sync.Mutex),
+		readers: readers,
 	}
+	combinedOut.combine()
 	return combinedOut
 }
 
-func syncCopy(reader io.Reader, writer io.Writer, locker sync.Locker) {
+func (reader *combinedReader) Read(p []byte) (n int, err error) {
+	numBytes, err := reader.Buffer.read(p)
+	if err != io.EOF {
+		return numBytes, err
+	}
+	// It is possible to return (0, nil), which is defined as a no-op, when the readers haven't been fully read
+	return numBytes, reader.exitError
+}
+
+func (reader *combinedReader) combine() {
+	doneChan := make(chan error, len(reader.readers))
+	for _, subReader := range reader.readers {
+		go func(subReader io.Reader) {
+			doneChan <- reader.syncCopy(subReader)
+		}(reader)
+	}
+
+	go reader.trackDone(doneChan)
+	return combinedOut
+}
+
+func (reader *combinedReader) trackDone(doneChan chan<- error) {
+	doneCount := 0
+	for doneCount < len(reader.readers) {
+		err := <-doneChan
+		doneCount++
+		if err != io.EOF {
+			reader.exitError = err
+			return
+		}
+	}
+	reader.exitError = io.EOF
+}
+
+func (reader *combinedReader) syncCopy(subReader io.Reader) error {
 	buffer := make([]byte, 1024)
 	for {
-		numBytes, err := reader.Read(buffer)
-		if numBytes == 0 || err == io.EOF {
-			return
-		} else if err != nil {
-			panic(err)
+		numBytes, err := subReader.Read(buffer)
+		if err != nil {
+			return err
 		}
-		syncWrite(writer, buffer[:numBytes], locker)
+		if numBytes > 0 {
+			err = reader.syncWrite(buffer[:numBytes])
+			if err != nil {
+				return err
+			}
+		}
 	}
 }
 
-func syncWrite(writer io.Writer, buffer []byte, locker sync.Locker) {
-	locker.Lock()
-	defer locker.Unlock()
+func (reader *combinedReader) syncWrite(buffer []byte) error {
+	reader.locker.Lock()
+	defer reader.locker.Unlock()
 
-	_, err := writer.Write(buffer)
-	if err != nil {
-		panic(err)
-	}
+	_, err := reader.Write(buffer)
+	return err
 }
