86305adc4f712bb3c6c7ffa7dd99627d9a57fc40
Jordan Potter <jordannpotter@gmail.com>
2013-12-06 15:08:53 -0800

    Adds sample text for populating database scripts

--- code/back/src/koality/resources/database/populateDatabase/populateDatabase.go
+++ code/back/src/koality/resources/database/populateDatabase/populateDatabase.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"koality/resources"
 	"koality/resources/database"
+	"math/rand"
 	"time"
 )
 
 const (
-	numRepositories = 3
+	numRepositories      = 3
+	numVerifications     = 100
+	parallelizationLevel = 4
 )
 
 func main() {
@@ -31,7 +34,29 @@ func main() {
 		panic(err)
 	}
 
-	var _ = repositoryIds
+	verificationIds, err := createVerifications(connection, repositoryIds)
+	if err != nil {
+		panic(err)
+	}
+
+	stageIds, err := createStages(connection, verificationIds)
+	if err != nil {
+		panic(err)
+	}
+
+	stageRunIds, err := createStageRuns(connection, stageIds)
+	if err != nil {
+		panic(err)
+	}
+
+	err = addConsoleText(connection, stageRunIds)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Completed populating database (%s)\n", getTimeString())
+
+	fmt.Printf("TODO: Saving dump (%s)\n", getTimeString())
 }
 
 func getTimeString() string {
@@ -67,7 +92,7 @@ func createUsers(connection *resources.Connection) error {
 }
 
 func createRepositories(connection *resources.Connection) ([]uint64, error) {
-	repositoryIds := make([]uint64, numRepositories)
+	repositoryIds := make([]uint64, 0, numRepositories)
 
 	for index := 0; index < numRepositories; index++ {
 		repositoryName := fmt.Sprintf("Repository %d", index)
@@ -77,8 +102,125 @@ func createRepositories(connection *resources.Connection) ([]uint64, error) {
 		if err != nil {
 			return nil, err
 		}
-		repositoryIds[index] = repositoryId
+		repositoryIds = append(repositoryIds, repositoryId)
 	}
 
 	return repositoryIds, nil
+}
+
+func createVerifications(connection *resources.Connection, repositoryIds []uint64) ([]uint64, error) {
+	verificationIds := make([]uint64, 0, numVerifications)
+
+	userNames := []string{"Jonathan Chu", "Jordan Potter", "Brian Bland", "Andrey Kostov"}
+	userEmails := []string{"jchu@koalitycode.com", "jpotter@koalitycode.com", "bbland@koalitycode.com", "akostov@koalitycode.com"}
+	mergeTargets := []string{"master", "development", "feature_branch_1", "feature_branch_2"}
+
+	createSha := func() string {
+		shaChars := "0123456789ABCDEF"
+		sha := ""
+		for index := 0; index < 40; index++ {
+			randomChar := shaChars[rand.Intn(len(shaChars))]
+			sha = sha + string(randomChar)
+		}
+		return sha
+	}
+
+	for _, repositoryId := range repositoryIds {
+		numVerificationsToCreate := numVerifications / numRepositories
+		if repositoryId == repositoryIds[len(repositoryIds)-1] {
+			numVerificationsToCreate = numVerifications/numRepositories + numVerifications%numRepositories
+		}
+
+		for index := 0; index < numVerificationsToCreate; index++ {
+			headMessage := fmt.Sprintf("This is a commit from %s", userNames[index%len(userNames)])
+			verificationId, err := connection.Verifications.Create.Create(repositoryId, createSha(), createSha(),
+				headMessage, userNames[index%len(userNames)], userEmails[index%len(userEmails)],
+				mergeTargets[index%len(mergeTargets)], userEmails[rand.Intn(len(userEmails))])
+			if err != nil {
+				return nil, err
+			}
+			verificationIds = append(verificationIds, verificationId)
+		}
+	}
+
+	return verificationIds, nil
+}
+
+func createStages(connection *resources.Connection, verificationIds []uint64) ([]uint64, error) {
+	stageNames := []string{"install dependencies", "prepare database", "frontend tests", "backend tests"}
+	stageIds := make([]uint64, 0, numVerifications*len(stageNames))
+
+	for _, verificationId := range verificationIds {
+		for index, stageName := range stageNames {
+			stageId, err := connection.Stages.Create.Create(verificationId, uint64(index/2), stageName, uint64(index%2))
+			if err != nil {
+				return nil, err
+			}
+			stageIds = append(stageIds, stageId)
+		}
+	}
+
+	return stageIds, nil
+}
+
+func createStageRuns(connection *resources.Connection, stageIds []uint64) ([]uint64, error) {
+	stageRunIds := make([]uint64, 0, len(stageIds)*parallelizationLevel)
+
+	getReturnCode := func() int {
+		if rand.Float32() > .2 {
+			return 0
+		} else {
+			return rand.Intn(254) + 1
+		}
+	}
+
+	for _, stageId := range stageIds {
+		stageRunId, err := connection.Stages.Create.CreateRun(stageId)
+		if err != nil {
+			return nil, err
+		}
+
+		err = connection.Stages.Update.SetStartTime(stageRunId, time.Now())
+		if err != nil {
+			return nil, err
+		}
+
+		err = connection.Stages.Update.SetReturnCode(stageRunId, getReturnCode())
+		if err != nil {
+			return nil, err
+		}
+
+		err = connection.Stages.Update.SetEndTime(stageRunId, time.Now())
+		if err != nil {
+			return nil, err
+		}
+
+		stageRunIds = append(stageRunIds, stageRunId)
+	}
+
+	return stageRunIds, nil
+}
+
+func addConsoleText(connection *resources.Connection, stageRunIds []uint64) error {
+	books := [][]string{iRobot, aliceInWonderland, greatExpectations}
+
+	textToTextMap := func(text []string) map[uint64]string {
+		textMap := make(map[uint64]string, len(text))
+		for lineNumber, line := range text {
+			textMap[uint64(lineNumber)] = line
+		}
+		return textMap
+	}
+
+	for _, stageRunId := range stageRunIds {
+		text := books[rand.Intn(len(books))]
+		maxLines := rand.Intn(len(text)-200) + 200
+		textToAdd := text[0:maxLines]
+		err := connection.Stages.Update.AddConsoleLines(stageRunId, textToTextMap(textToAdd))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
