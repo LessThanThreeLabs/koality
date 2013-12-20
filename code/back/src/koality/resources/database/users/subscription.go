a97aeca1d4417af611a184c0392ff9725ddf8fed
Jordan Potter <jordannpotter@gmail.com>
2013-12-20 12:23:47 -0800

    Implements the remaining user subscription calls

--- code/back/src/koality/resources/database/users/subscription.go
+++ code/back/src/koality/resources/database/users/subscription.go
@@ -18,19 +18,27 @@ func NewSubscriptionHandler() (resources.UsersSubscriptionHandler, error) {
 }
 
 func (subscriptionHandler *SubscriptionHandler) SubscribeToUserCreatedEvents(updateHandler resources.UserCreatedHandler) (resources.SubscriptionId, error) {
-	return 0, nil
+	return subscriptionHandler.createdSubscriptionManager.Add(updateHandler)
 }
 
 func (subscriptionHandler *SubscriptionHandler) UnsubscribeFromUserCreatedEvents(subscriptionId resources.SubscriptionId) error {
-	return nil
+	return subscriptionHandler.createdSubscriptionManager.Remove(subscriptionId)
+}
+
+func (subscriptionHandler *SubscriptionHandler) fireCreatedEvent(userId uint64, firstName, lastName string, admin bool) {
+	subscriptionHandler.createdSubscriptionManager.Fire(userId, firstName, lastName, admin)
 }
 
 func (subscriptionHandler *SubscriptionHandler) SubscribeToUserDeletedEvents(updateHandler resources.UserDeletedHandler) (resources.SubscriptionId, error) {
-	return 0, nil
+	return subscriptionHandler.deletedSubscriptionManager.Add(updateHandler)
 }
 
 func (subscriptionHandler *SubscriptionHandler) UnsubscribeFromUserDeletedEvents(subscriptionId resources.SubscriptionId) error {
-	return nil
+	return subscriptionHandler.deletedSubscriptionManager.Remove(subscriptionId)
+}
+
+func (subscriptionHandler *SubscriptionHandler) fireDeletedEvent(userId uint64) {
+	subscriptionHandler.deletedSubscriptionManager.Fire(userId)
 }
 
 func (subscriptionHandler *SubscriptionHandler) SubscribeToNameEvents(updateHandler resources.UserNameUpdateHandler) (resources.SubscriptionId, error) {
@@ -46,25 +54,37 @@ func (subscriptionHandler *SubscriptionHandler) fireNameEvent(userId uint64, fir
 }
 
 func (subscriptionHandler *SubscriptionHandler) SubscribeToAdminEvents(updateHandler resources.UserAdminUpdateHandler) (resources.SubscriptionId, error) {
-	return 0, nil
+	return subscriptionHandler.adminUpdatedSubscriptionManager.Add(updateHandler)
 }
 
 func (subscriptionHandler *SubscriptionHandler) UnsubscribeFromAdminEvents(subscriptionId resources.SubscriptionId) error {
-	return nil
+	return subscriptionHandler.adminUpdatedSubscriptionManager.Remove(subscriptionId)
+}
+
+func (subscriptionHandler *SubscriptionHandler) fireAdminEvent(userId uint64, admin bool) {
+	subscriptionHandler.adminUpdatedSubscriptionManager.Fire(userId, admin)
 }
 
 func (subscriptionHandler *SubscriptionHandler) SubscribeToSshKeyAddedEvents(updateHandler resources.UserSshKeyAddedHandler) (resources.SubscriptionId, error) {
-	return 0, nil
+	return subscriptionHandler.sshKeyAddedSubscriptionManager.Add(updateHandler)
 }
 
 func (subscriptionHandler *SubscriptionHandler) UnsubscribeFromSshKeyAddedEvents(subscriptionId resources.SubscriptionId) error {
-	return nil
+	return subscriptionHandler.sshKeyAddedSubscriptionManager.Remove(subscriptionId)
+}
+
+func (subscriptionHandler *SubscriptionHandler) fireSshKeyAddedEvent(userId, sshKeyId uint64, name string) {
+	subscriptionHandler.sshKeyAddedSubscriptionManager.Fire(userId, sshKeyId, name)
 }
 
 func (subscriptionHandler *SubscriptionHandler) SubscribeToSshKeyRemovedEvents(updateHandler resources.UserSshKeyRemovedHandler) (resources.SubscriptionId, error) {
-	return 0, nil
+	return subscriptionHandler.sshKeyRemovedSubscriptionManager.Add(updateHandler)
 }
 
 func (subscriptionHandler *SubscriptionHandler) UnsubscribeFromSshKeyRemovedEvents(subscriptionId resources.SubscriptionId) error {
-	return nil
+	return subscriptionHandler.sshKeyRemovedSubscriptionManager.Remove(subscriptionId)
+}
+
+func (subscriptionHandler *SubscriptionHandler) fireSshKeyRemovedEvent(userId, sshKeyId uint64) {
+	subscriptionHandler.sshKeyRemovedSubscriptionManager.Fire(userId, sshKeyId)
 }
