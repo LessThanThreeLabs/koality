442754473d086a34d5d409bbe4a8b534938e4dd5
Brian Bland <r4nd0m1n4t0r@gmail.com>
2014-01-13 15:50:15 -0800

    Adds format-string log methods

--- code/back/src/koality/util/log/log.go
+++ code/back/src/koality/util/log/log.go
@@ -31,18 +31,38 @@ func Debug(v ...interface{}) {
 	Logger.Log(log.Levels.Debug, fmt.Sprintln(v...))
 }
 
+func Debugf(format strint, v ...interface{}) {
+	Logger.Log(log.Levels.Debug, fmt.Sprintf(format+"\n", v...))
+}
+
 func Info(v ...interface{}) {
 	Logger.Log(log.Levels.Info, fmt.Sprintln(v...))
+}
+
+func Infof(format strint, v ...interface{}) {
+	Logger.Log(log.Levels.Info, fmt.Sprintf(format+"\n", v...))
 }
 
 func Warning(v ...interface{}) {
 	Logger.Log(log.Levels.Warning, fmt.Sprintln(v...))
 }
 
+func Warningf(format strint, v ...interface{}) {
+	Logger.Log(log.Levels.Warning, fmt.Sprintf(format+"\n", v...))
+}
+
 func Error(v ...interface{}) {
 	Logger.Log(log.Levels.Error, fmt.Sprintln(v...))
 }
 
+func Errorf(format strint, v ...interface{}) {
+	Logger.Log(log.Levels.Error, fmt.Sprintf(format+"\n", v...))
+}
+
 func Critical(v ...interface{}) {
 	Logger.Log(log.Levels.Critical, fmt.Sprintln(v...))
+}
+
+func Criticalf(format strint, v ...interface{}) {
+	Logger.Log(log.Levels.Critical, fmt.Sprintf(format+"\n", v...))
 }
