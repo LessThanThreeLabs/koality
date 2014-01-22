dcd0d4bfcc1c76c44386c0961057fa903eddf7db
Brian Bland <r4nd0m1n4t0r@gmail.com>
2014-01-21 20:00:57 -0800

    Uses GOPATH environment variable for bin pathing for stageRunner tests
    Also sets parallelization to 2

--- code/back/src/koality/verification/stagerunner/stageRunner_test.go
+++ code/back/src/koality/verification/stagerunner/stageRunner_test.go
@@ -17,9 +17,7 @@ import (
 	"koality/vm/localmachine"
 	"os"
 	"os/exec"
-	"os/user"
 	"path"
-	"path/filepath"
 	"testing"
 )
 
@@ -101,16 +99,17 @@ func (suite *StageRunnerSuite) TestExporting(check *gocheck.C) {
 		"AKIAJIXWHV32ZY75SQBQ", "JgD4KK376m9Z3E3MjMt8YcPg3cuzl958Qjtbrht1", "koality-whim")
 	check.Assert(err, gocheck.IsNil)
 
-	copyExec, err := suite.virtualMachine.FileCopy("$HOME/code/back/src/koality/util/xunitsamples/*.xml", "xunitPath/")
+	copyExec, err := suite.virtualMachine.FileCopy(path.Join(os.Getenv("GOPATH"), "src", "koality", "util", "xunitsamples", "*.xml"), "xunitPath/")
 	check.Assert(err, gocheck.IsNil)
 
 	err = copyExec.Run()
 	check.Assert(err, gocheck.IsNil)
 
-	exportPathsBinPath := "/home/koality/code/back/src/koality/util/exportPaths"
+	exportPathsBinPath := path.Join(os.Getenv("GOPATH"), "src", "koality", "util", "exportPaths")
 	compileCmd := exec.Command("go", "build", "-o",
 		exportPathsBinPath,
-		"/home/koality/code/back/src/koality/util/exportPaths.go")
+		path.Join(os.Getenv("GOPATH"), "src", "koality", "util", "exportPaths.go"),
+	)
 	err = compileCmd.Run()
 	check.Assert(err, gocheck.IsNil)
 
@@ -123,11 +122,9 @@ func (suite *StageRunnerSuite) TestExporting(check *gocheck.C) {
 	check.Assert(err, gocheck.IsNil)
 
 	commandGroup := commandgroup.New(commands)
-	usr, err := user.Current()
-	check.Assert(err, gocheck.IsNil)
 
 	exportPaths := []string{
-		path.Join(usr.HomeDir, "code", "back", "src", "koality", "util", "xunitsamples"),
+		path.Join(os.Getenv("GOPATH"), "src", "koality", "util", "xunitsamples"),
 	}
 
 	testSection := section.New("test", false, section.RunOnAll, section.FailOnAny, false, nil, commandGroup, exportPaths)
@@ -161,7 +158,7 @@ func (suite *StageRunnerSuite) TestExporting(check *gocheck.C) {
 	// check.Assert(exports, gocheck.HasLen, 1)
 	check.Assert(len(exports), gocheck.Equals, 1)
 	expectedBucket := "koality-whim"
-	expectedPath := "/home/koality/code/back/src/koality/util/xunitsamples/sample1.xml"
+	expectedPath := path.Join(os.Getenv("GOPATH"), "src", "koality", "util", "xunitsamples", "sample1.xml")
 	expectedKey :=
 		fmt.Sprintf("repository/%d/verification/%d/stage/%d/stageRun/%d%s",
 			suite.repository.Id, suite.verification.Id, stage.Id,
@@ -174,16 +171,17 @@ func (suite *StageRunnerSuite) TestExporting(check *gocheck.C) {
 }
 
 func (suite *StageRunnerSuite) TestXunitParser(check *gocheck.C) {
-	copyExec, err := suite.virtualMachine.FileCopy("$HOME/code/back/src/koality/util/xunitsamples/*.xml", "xunitPath/")
+	copyExec, err := suite.virtualMachine.FileCopy(path.Join(os.Getenv("GOPATH"), "src", "koality", "util", "xunitsamples", "*.xml"), "xunitPath/")
 	check.Assert(err, gocheck.IsNil)
 
 	err = copyExec.Run()
 	check.Assert(err, gocheck.IsNil)
 
-	getXunitResultsBinPath := "/home/koality/code/back/src/koality/util/getXunitResults"
+	getXunitResultsBinPath := path.Join(os.Getenv("GOPATH"), "src", "koality", "util", "getXunitResults")
 	compileCmd := exec.Command("go", "build", "-o",
 		getXunitResultsBinPath,
-		"/home/koality/code/back/src/koality/util/getXunitResults.go")
+		path.Join(os.Getenv("GOPATH"), "src", "koality", "util", "getXunitResults.go"),
+	)
 	err = compileCmd.Run()
 	check.Assert(err, gocheck.IsNil)
 
@@ -224,10 +222,7 @@ func (suite *StageRunnerSuite) TestXunitParser(check *gocheck.C) {
 	xunitResults, err := suite.resourcesConnection.Stages.Read.GetAllXunitResults(stageRuns[0].Id)
 	check.Assert(err, gocheck.IsNil)
 
-	usr, err := user.Current()
-	check.Assert(err, gocheck.IsNil)
-
-	xunitPath := path.Join(usr.HomeDir, "code", "back", "src", "koality", "util", "xunitsamples")
+	xunitPath := path.Join(os.Getenv("GOPATH"), "src", "koality", "util", "xunitsamples")
 	expectedXunitBytes, err := xunit.GetXunitResults("*.xml", []string{xunitPath}, ioutil.Discard, os.Stderr)
 	check.Assert(err, gocheck.IsNil)
 
@@ -237,10 +232,10 @@ func (suite *StageRunnerSuite) TestXunitParser(check *gocheck.C) {
 
 	// normalize paths
 	for i := range xunitResults {
-		xunitResults[i].Path = filepath.Base(xunitResults[i].Path)
+		xunitResults[i].Path = path.Base(xunitResults[i].Path)
 	}
 	for i := range expectedXunitResults {
-		expectedXunitResults[i].Path = filepath.Base(expectedXunitResults[i].Path)
+		expectedXunitResults[i].Path = path.Base(expectedXunitResults[i].Path)
 	}
 	check.Assert(xunitResults, gocheck.Equals, expectedXunitResults)
 
