d87951bf6b012b35ff0f7eddca2a61510fe81830
Donald Huang <don.hcd@gmail.com>
2014-01-24 15:47:57 -0800

    added an ssh wrapper binary that takes private key as env var

---
+++ code/back/src/koality/util/sshwrapper/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"syscall"
+)
+
+func main() {
+	file, err := ioutil.TempFile("", "")
+	if err != nil {
+		panic(err)
+	}
+
+	filename := file.Name()
+	if _, err = file.WriteString(os.Getenv("PRIVATE_KEY")); err != nil {
+		panic(err)
+	}
+
+	if err = file.Chmod(0600); err != nil {
+		panic(err)
+	} else if err = file.Close(); err != nil {
+		panic(err)
+	}
+
+	bashPath := path.Join("/bin", "bash")
+	if err = exec.Command(bashPath, "-c", "sleep 120; rm "+filename).Start(); err != nil {
+		panic(err)
+	}
+
+	sshPath, err := exec.LookPath("ssh")
+	if err != nil {
+		panic(err)
+	}
+
+	if sshPath, err = filepath.Abs(sshPath); err != nil {
+		panic(err)
+	}
+
+	argv := append(append([]string{sshPath}, "-i", filename), os.Args[1:]...)
+	panic(syscall.Exec(argv[0], argv, nil))
+}
